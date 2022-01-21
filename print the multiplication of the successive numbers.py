2a5eafcf9592fcce705b8de987641dd29f9e8937
Harish V <97931220+Harish08112007@users.noreply.github.com>
2022-01-21 14:11:47 +0530

    Add files via upload

--- print the multiplication of the successive numbers.py
+++ print the multiplication of the successive numbers.py
@@ -1,3 +1,10 @@
-a=int(input("Enter a Number"))
-for i in range(1,a+1):
-    
+n=int(input("Enter a Number: "))
+d=1
+x=[]
+def multiply(x,d):
+    for i in range(1,n+1):
+        x.append(i)
+    for a in x:
+        d=d*a
+    return d
+print("The predisissors of entred number multiplied is", multiply(x,d))
